9abad042c4a1032c52d77ddc476bade40ed7a674
OuraaD <ouradiarra2@gmail.com>
2024-09-17 16:18:48 +0200

    view recipe

--- src/Controller/Admin/IngredientController.php
+++ src/Controller/Admin/IngredientController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-// #[IsGranted('ROLE_ADMIN')]
+
 #[Route('/admin/ingredient', name: 'admin_ingredient_')]
 class IngredientController extends AbstractController
 {
